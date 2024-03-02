034d64de3b01bcd47e6fc457e25272eed1ceb372
RobertRuai <robertruai4@gmail.com>
2024-03-02 17:39:22 +0300

    created the signup and login forms

---
+++ pos/authentication/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import *
+
+from django import forms
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput, EmailInput
+
+
+# - Register/Create a user
+class CreateUserForm(UserCreationForm):
+    email = forms.EmailField(widget=EmailInput())
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+# - Login a user
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
