153883d7b5f80c95ad34e109333af49d21a13562
RobertRuai <robertruai4@gmail.com>
2024-03-11 15:19:38 +0300

    added the signup and login functions

--- pos/authentication/views.py
+++ pos/authentication/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, SignUpForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register_user(request):
+    msg = None
+    success = False
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            messages.succes(request, 'User created - please <a href="/login">login</a>.')
+
+            # return redirect("/login/")
+
+        else:
+            messages.error(request, 'Form is not valid')
+    else:
+        form = SignUpForm()
+
+    return render(request, "accounts/register.html", {"form": form})
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                messages.error(request, 'Invalid username or password!')
+        else:
+            messages.eror(request, 'An error occurred!.')
+
+    return render(request, "accounts/login.html", {"form": form})
